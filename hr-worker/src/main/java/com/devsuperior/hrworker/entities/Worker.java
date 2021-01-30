16dab74435d28c7ec1f5c45c97ffa0f581269fa7
Fabricio Ramos <fabricio-beco@hotmail.com>
2021-01-30 14:47:57 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -0,0 +1,34 @@
+package com.devsuperior.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private Double dailyIncome;
+
+  public Worker() {
+  }
+
+  public Worker(Long id, String name, Double dailyIncome) {
+    this.id = id;
+    this.name = name;
+    this.dailyIncome = dailyIncome;
+  }
+
+}
---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -0,0 +1,36 @@
+package com.devsuperior.hrworker.resources;
+
+import java.util.List;
+
+import com.devsuperior.hrworker.entities.Worker;
+import com.devsuperior.hrworker.repositories.WorkerRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("workers")
+public class WorkerResource {
+
+  private WorkerRepository repository;
+
+  public WorkerResource(WorkerRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Worker>> findAll() {
+    List<Worker> list = repository.findAll();
+    return ResponseEntity.ok(list);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Worker> findById(@PathVariable Long id) {
+    Worker obj = repository.findById(id).get();
+    return ResponseEntity.ok(obj);
+  }
+
+}
